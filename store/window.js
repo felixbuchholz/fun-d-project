4bb7d1869ce01adef0abfc7a734779b83250bfa7
Isabel Verkes <isabelverkes@Karel.local>
2019-10-12 16:04:33 -0400

    hallo first time

--- store/window.js
+++ store/window.js
@@ -1,7 +1,9 @@
+// passive state
 export const state = () => ({
   window: { width: 360, height: 640, animationFrame: 20 }
 });
 
+// Automatic active: in case something changes in the state, this is a recalculation
 export const getters = {
   width(state) {
     return state.window.width - state.window.animationFrame * 2;
@@ -11,6 +13,7 @@ export const getters = {
   }
 };
 
+// Tool to change. In case you somewhere in the program want to change something, this responds
 export const mutations = {
   CHANGE_WINDOW(state, obj) {
     state.window[obj.property] = obj.value;
