01f5fcf3015074cf86c28ee025d14ad2c1d05a74
Isabel Verkes <isaverkes@hotmail.com>
2019-11-20 12:43:17 -0500

    first draft intro done - needs layout work

--- components/intro/Intro.vue
+++ components/intro/Intro.vue
@@ -14,7 +14,7 @@
       
       <div class="wrap-collabsible">
         <input id="collapsible" class="toggle" type="checkbox">
-        <label for="collapsible" class="lbl-toggle">Proxy advisors?</label>
+        <label for="collapsible" class="lbl-toggle">What are proxy advisors?</label>
           <div class="collapsible-content">
             <div class="content-inner">
               <p>
@@ -23,7 +23,42 @@
             </div>
           </div>
         </div> <!-- end div collapse -->
-        
+        <p>This ignited concerns that the rise of passive investing translated into growing passivity of shareholders towards management.</p>
+      
+      <div class="wrap-collabsible">
+        <input id="collapsible-2" class="toggle" type="checkbox">
+        <label for="collapsible-2" class="lbl-toggle">Why management vs shareholders?</label>
+          <div class="collapsible-content">
+            <div class="content-inner">
+              <p>
+                A company’s management team acts as agents for the owners (shareholders) of a company. Conflicts of interests arise when a managers’ own interests contrast with those of shareholders. And different shareholders might have conflicting ideas on what is best for a company. Annual shareholder meetings are an opportunity for a company’s shareholders to raise issues that they could not reach consensus on with that company’s management. Activists seek to confront management by submitting proposals for reform on a variety issues, such as removal of certain directors, executive compensation or disclosure of climate change risks. Sometimes for reasons selfish to those shareholders, sometimes to benefit a larger set of stakeholders. Index funds are not activists, in that they don’t bring their own resolutions to the table, nor publicly attack management to provoke a specific change.
+              </p>
+            </div>
+          </div>
+        </div> <!-- end div collapse -->
+        <p>In voting on proposals, often the most telling cases are when asset managers get to choose between supporting management or activist shareholders. Indeed, even though most shareholder proposals don’t pass, the issue being on the agenda can already ignite a debate.</p>
+        <p>However, an excessive passivity of the largest asset managers in scrutinizing management would defeat the option for shareholders to hold management accountable. Indirectly this would forfeit the opportunity to vote for all people that have (a tiny) share in a fund.</p>
+        <p>The passive giants - Vanguard, State Street and Blackrock - are all attentive to this concern. Each of them explicitly emphasize and explicate their proactive approach in engaging with the companies in their funds. However, it remains difficult for investors in index funds to assess how their approach plays out in practice.</p>
+        <br>
+        <h3>The future of capitalism </h3>
+        <p>Financial markets are showing more interest in issues such as  <a href="https://www.unepfi.org/news/themes/climate-change/leading-financial-firms-commit-to-improved-transparency-on-the-risks-of-climate-change/" target="_blank">climate change</a> and <a href="https://www.unpri.org/download?ac=5599" target="_blank">income inequality</a>. Rather than focusing on shareholder returns only, there is a growing public demand for companies to increase corporate accountability. The substantial voting power of the three largest asset managers puts them in a spotlight. They can often tip the needle on these more “societal” proposals brought by shareholders. As law professor John Morley writes: “The future of American capitalism belongs to a handful of giant investment managers”.<a href="https://papers.ssrn.com/sol3/Delivery.cfm/SSRN_ID3384274_code669617.pdf?abstractid=3225555&amp;mirid=1" target="_blank">  &#9758;   </a> </p>
+        <p> So, the voting behavior of the three largest asset managers matters. Every mutual fund is required to publish annually how it casted each vote on all proposals for all the portfolio companies. Even though the data is public, its published in a format that even investment fund enthusiasts will find obscure. For the millions of mainstream fund investors (such as everyone who has some form of retirement plan), the public data barely give a clear picture of how the funds they invested in voted. Also <i class="qtip tip-top" data-tip="Securities and Exchange Commission (“SEC”) is the US’ federal regulator, overseeing securities markets"> SEC</i> Commissioner Robert Jackson previously called out the lack of transparency of how index funds votes. <a href="https://www.cnbc.com/video/2018/12/13/sec-commissioner-investors-need-more-transparency.html" target="_blank">  &#9758;   </a>. 
+        </p>
+        <br>
+        <h3>Engaged shareholders</h3>
+        <p>This visualization addresses this concern by providing a peek into voting patterns of the world’s largest financial shareholders: asset management firms Blackrock, Vanguard and State Street.Most proposals at shareholder meetings come from management and cover organizational issues such as the appointment of an accountant or board directors. Shareholders (“activists”) can submit proposals as well as a means to provoke a certain course of action by the company</p>
+            <div class="wrap-collabsible">
+        <input id="collapsible-3" class="toggle" type="checkbox">
+        <label for="collapsible-3" class="lbl-toggle">Which shareholders can submit proposals?</label>
+          <div class="collapsible-content">
+            <div class="content-inner">
+              <p>
+                Eligibility rules of the US Securities and Exchange Commission (SEC) state that shareholders who own more than $2,000 in stock or 1% of the company are permitted to initiate a shareholder proposal, whichever is lower. In November 2019, the SEC proposed to increase that threshold to prevent the inappropriate or excessive use of company resources and waste of shareholders’ time and attention. Not everyone agrees with that.
+              </p>
+            </div>
+          </div>
+        </div> <!-- end div collapse -->
+        <p>Unless the SEC allows a company to exempt a shareholder resolution from the agenda, it has to be placed on the agenda for the next available shareholder meeting. Shareholders may try to reach consensus with management through informal dialogue and (sometimes aggressive) public campaigns to move management’s position. The largest asset managers will oftentimes be involved in these cases, as a form of “shareholder engagement”. Since this happens behind closed doors, assessing this “soft” influence is impossible. Our analysis therefore looks at the cases where activist shareholders come to open confrontation with management and arrive at a proxy vote at the shareholder meeting. As follows, we explore how the world’s largest asset managers voted on shareholder proposals as an explicit expression of their influence.</p>
    </div>  <!-- end div of flex container -->
 
   
