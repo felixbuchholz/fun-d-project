697c342a36dd9f62e3aee1e3c60122092c5d14ef
felixbuchholz <felixbuchholz@gmx.net>
2019-11-20 22:26:53 -0500

    add switch pattern for scrolly telling

--- components/visualizations/VisualizationText.vue
+++ components/visualizations/VisualizationText.vue
@@ -10,15 +10,29 @@
 <script>
 export default {
   methods: {
+    setActiveCats(array) {
+      this.$store.commit("proposals/SET_ACTIVE_CATEGORIES", array);
+    },
     stepEnterHandler(event) {
-      console.log(event);
-      if (event.index == 1) {
-        this.$store.commit("proposals/SET_ACTIVE_CATEGORIES", ["soc"]);
-      } else if (event.index == 2) {
-        this.$store.commit("proposals/SET_ACTIVE_CATEGORIES", [
-          "env",
-          "profit"
-        ]);
+      const up = event.direction == "up";
+      const down = event.direction == "down";
+      switch (event.index) {
+        case 1:
+          if (down) {
+            console.log("1 down");
+            this.setActiveCats(["env"]);
+          } else if (up) {
+            console.log("1 up");
+            this.setActiveCats([]);
+          }
+          break;
+        case 2:
+          if (down) {
+            console.log("2 down");
+          } else if (up) {
+            console.log("2 down");
+          }
+          break;
       }
     }
   }
